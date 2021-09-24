d4e3a9f64d310a39352246391880a6db1bc5fdea
Artem <a.yasenko@gmail.com>
2021-09-24 20:26:32 +0300

    Fifteen game added

---
+++ fifteen-game/precache-manifest.1d3050dbaa0c0f68f54dd46a2cc90760.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4c94318dd73f610a16b07502f3663e4b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9c72919214134ee337ae",
+    "url": "/static/css/main.68c6890e.chunk.css"
+  },
+  {
+    "revision": "2c35752608669b39a5fb",
+    "url": "/static/js/2.304b69ad.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.304b69ad.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9c72919214134ee337ae",
+    "url": "/static/js/main.fe7b17f5.chunk.js"
+  },
+  {
+    "revision": "a963b3982464f387b7a8",
+    "url": "/static/js/runtime-main.5ec748c3.js"
+  }
+]);
